da875f82e4ba2ef6a12c05c1b7481f2f52459470
Александр <166290396+AlecsandrTrapeznikov@users.noreply.github.com>
2024-04-14 14:27:36 +0500

    Update asdsdsd.py

--- asdsdsd.py
+++ asdsdsd.py
@@ -5,12 +5,19 @@ window = display.set_mode((600,600))
 fps = time.Clock()
 game = True
 img = Surface((20, 150))
+img2 = Surface((20, 150))
+x = 20
+y = 10
+x1 = 560
+y1 = 10
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        
     window.fill((255,255,255))
-    window.blit(img, (5,5))
+    window.blit(img, (x,y))
+    window.blit(img2, (x1,y1))
 
     display.update()
